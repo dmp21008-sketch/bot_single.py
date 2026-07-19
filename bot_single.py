57253fa8f182b4500f4e27d977db211be260430d
dmp21008-sketch <dmp21008@gmail.com>
2026-07-19 17:49:27 +0700

    Update bot_single.py

--- bot_single.py
+++ bot_single.py
@@ -54,10 +54,10 @@ from sqlalchemy.orm import (
 )
 
 # ── Cấu hình Shop & Tài Xỉu ───────────────────────────────────────────────────
-ACCOUNT_PRICE = 200  # VNĐ mỗi acc[span_7](start_span)[span_7](end_span)
+ACCOUNT_PRICE = 300  # VNĐ mỗi acc[span_7](start_span)[span_7](end_span)
 MIN_ORDER_QTY = 50  # Số lượng tối thiểu[span_8](start_span)[span_8](end_span)
 CHECKER_LINK = "t.me/tretrauchecker_bot?start=_tgr_8UulJtkyZjE1"  #[span_9](start_span)[span_9](end_span)
-XU_DIEMDANH = 50  # Số xu nhận được khi điểm danh[span_10](start_span)[span_10](end_span)
+XU_DIEMDANH = 500  # Số xu nhận được khi điểm danh[span_10](start_span)[span_10](end_span)
 THUONG_REF_XU = 5000  # Thưởng mời bạn bè chơi bot
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  #[span_11](start_span)[span_11](end_span)
